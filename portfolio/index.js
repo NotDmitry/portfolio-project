52c70cd02fb808c9f5bf825e2e2bde0fd93b85e7
Dzmirty Buldyha <buldygodmitrij@gmail.com>
2025-10-03 23:28:36 +0300

    feat: add complete modal window behaviour

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,8 +1,10 @@
-const modal = document.querySelector('.modal-window');
+const modal = document.querySelector('dialog');
+const closeButton = document.querySelector('.close-button');
 let buttons = document.querySelectorAll('button');
 buttons = Array.from(buttons);
-buttons.pop();
+buttons.splice(-2);
 
+// Modal window
 buttons.forEach(button => {
   button.addEventListener('click', (e) => {
     document.body.classList.toggle('fixed-body');
@@ -10,4 +12,8 @@ buttons.forEach(button => {
   })
 })
 
-modal.addEventListener('close', (e) => document.body.classList.toggle('fixed-body'));
+modal.addEventListener('click', (e) => {
+  if (e.target.contains(modal)) modal.close();
+})
+closeButton.addEventListener('click', () => modal.close());
+modal.addEventListener('close', () => document.body.classList.toggle('fixed-body'));
