bb1ae968b750b71a9a26a2517638a6b2ab3c913e
Dzmirty Buldyha <buldygodmitrij@gmail.com>
2025-10-05 15:17:59 +0300

    feat: add accordion state management

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,10 +1,8 @@
+// Modal window
 const modal = document.querySelector('dialog');
 const closeButton = document.querySelector('.close-button');
-let buttons = document.querySelectorAll('button');
-buttons = Array.from(buttons);
-buttons = buttons.slice(1, -2);
+const buttons = Array.from(document.querySelectorAll('button')).slice(1, -2);
 
-// Modal window
 buttons.forEach(button => {
   button.addEventListener('click', (e) => {
     document.body.classList.toggle('fixed-body');
@@ -12,10 +10,25 @@ buttons.forEach(button => {
   })
 })
 
+modal.addEventListener('close', () => document.body.classList.toggle('fixed-body'));
 modal.addEventListener('click', (e) => {
   if (e.target.contains(modal)) modal.close();
 })
 closeButton.addEventListener('click', () => modal.close());
-modal.addEventListener('close', () => document.body.classList.toggle('fixed-body'));
 
 // Accordion
+const accordionQuestions = Array.from(document.querySelectorAll('details'));
+
+const activeQuestion = +sessionStorage.getItem('question');
+if (activeQuestion !== -1) {
+  accordionQuestions[activeQuestion].setAttribute('open', '');
+} else {
+  accordionQuestions[0].removeAttribute('open');
+}
+
+accordionQuestions.forEach((question, _, accordion) => {
+  question.addEventListener('toggle', () => {
+    const openId = accordion.findIndex((question) => question.open);
+    sessionStorage.setItem('question', String(openId));
+  })
+})
